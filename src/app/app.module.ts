2c4a1c507f424a71f72783b558d1065644667a19
Ammar Botonjic <ammar.botonjic@digacon-software.com>
2024-02-11 19:14:49 +0100

    Generate navabar component and types

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArtCollectionComponent } from './art-collection/art-collection.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
